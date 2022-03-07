fac102086d9ddc2a75bb6a49e0442d36289ee3f8
suwidnyana <grayfullbuster522@gmail.com>
2022-03-07 13:06:33 +0800

    finish refactor

--- components/organisms/TransactionContent/index.tsx
+++ components/organisms/TransactionContent/index.tsx
@@ -1,33 +1,27 @@
 import React from 'react';
-import Category from './Category';
+import ButtonTab from './ButtonTab';
 import TableRow from './TableRow';
 
-export default function OverviewContent() {
+export default function TransactionContent() {
   return (
     <main className="main-wrapper">
       <div className="ps-lg-0">
-        <h2 className="text-4xl fw-bold color-palette-1 mb-30">Overview</h2>
-        <div className="top-up-categories mb-30">
-          <p className="text-lg fw-medium color-palette-1 mb-14">
-            Top Up Categories
-          </p>
-          <div className="main-content">
-            <div className="row">
-              <Category nominal={8455000} icon="ic-desktop">
-                Game
-                <br />
-                Destkop
-              </Category>
-              <Category nominal={8455000} icon="ic-mobile">
-                Game
-                <br />
-                Mobile
-              </Category>
-              <Category nominal={8455000} icon="ic-desktop">
-                Other
-                <br />
-                Catgories
-              </Category>
+        <h2 className="text-4xl fw-bold color-palette-1 mb-30">
+          My Transactions
+        </h2>
+        <div className="mb-30">
+          <p className="text-lg color-palette-2 mb-12">You’ve spent</p>
+          <h3 className="text-5xl fw-medium color-palette-1">
+            Rp 4.518.000.500
+          </h3>
+        </div>
+        <div className="row mt-30 mb-20">
+          <div className="col-lg-12 col-12 main-content">
+            <div id="list_status_title">
+              <ButtonTab title="All Trx" active />
+              <ButtonTab title="Success" active={false} />
+              <ButtonTab title="Pending" active={false} />
+              <ButtonTab title="Failed" active={false} />
             </div>
           </div>
         </div>
@@ -39,46 +33,47 @@ export default function OverviewContent() {
             <table className="table table-borderless">
               <thead>
                 <tr className="color-palette-1">
-                  <th className="text-start" scope="col">
+                  <th className="" scope="col">
                     Game
                   </th>
                   <th scope="col">Item</th>
                   <th scope="col">Price</th>
                   <th scope="col">Status</th>
+                  <th scope="col">Action</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody id="list_status_item">
                 <TableRow
                   image="overview-1"
                   title="Mobile Legend"
-                  categori="Mobile"
-                  item={200}
+                  category="Mobile"
+                  item="200"
                   price={29000}
-                  status="Pending"
+                  status="pending"
                 />
                 <TableRow
                   image="overview-2"
                   title="Call of Duty:Modern"
-                  categori="Desktop"
-                  item={200}
+                  category="Desktop"
+                  item="200"
                   price={29000}
-                  status="Pending"
+                  status="pending"
                 />
                 <TableRow
                   image="overview-3"
                   title="Clash Of Clans"
-                  categori="Mobile"
-                  item={200}
+                  category="Mobile"
+                  item="200"
                   price={29000}
-                  status="Pending"
+                  status="pending"
                 />
                 <TableRow
                   image="overview-4"
                   title="Valorant"
-                  categori="Mobile"
-                  item={200}
+                  category="Mobile"
+                  item="200"
                   price={29000}
-                  status="Success"
+                  status="success"
                 />
               </tbody>
             </table>
