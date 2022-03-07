585706586d5cbbf42340433f10757480d6d20df5
suwidnyana <grayfullbuster522@gmail.com>
2022-03-07 11:17:32 +0800

    refactor sidebar component

---
+++ components/organisms/Sidebar/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Footer from './Footer';
+import MenuItem from './MenuItem';
+import Profile from './Profile';
+
+interface SideBarProps {
+  activeMenu: 'overview' | 'transactions' | 'settings';
+}
+
+export default function Sidebar(props: SideBarProps) {
+  const { activeMenu } = props;
+  return (
+    <section className="sidebar">
+      <div className="content pt-50 pb-30 ps-30">
+        <Profile />
+        <div className="menus">
+          <MenuItem
+            title="Overview"
+            icon="ic-menu-overview"
+            active={activeMenu === 'overview'}
+            href="/member"
+          />
+          <MenuItem
+            title="Transaction"
+            icon="ic-menu-transaction"
+            active={activeMenu === 'transactions'}
+            href="/member/transactions"
+          />
+          <MenuItem title="Messages" icon="ic-menu-messages" href="/member" />
+          <MenuItem title="Card" icon="ic-menu-card" href="/member" />
+          <MenuItem title="Reward" icon="ic-menu-reward" href="/member" />
+          <MenuItem
+            title="Settings"
+            icon="ic-menu-setting"
+            active={activeMenu === 'settings'}
+            href="/member/edit-profile"
+          />
+          <MenuItem title="Log Out" icon="ic-menu-logout" />
+        </div>
+        <Footer />
+      </div>
+    </section>
+  );
+}
