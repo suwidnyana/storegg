ddbb182d07b82b962967110127ee006c5f7c01bc
Suwidnyana Putra <grayfullbuster522@gmail.com>
2023-01-10 00:41:39 +0800

    get value select option

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -1,6 +1,26 @@
 import Image from 'next/image';
+import { useCallback, useEffect, useState } from 'react';
+import { getGameCategory } from 'services/player';
 
 export default function SignUpPhoto() {
+  const [categories, setCategory] = useState([]);
+  const [favorite, setFavorite] = useState('');
+
+  const getGameCategoryAPI = useCallback(async () => {
+    const data = await getGameCategory();
+    console.log('data :', data);
+    setCategory(data);
+    setFavorite(data[0]._id);
+  }, [getGameCategory]);
+
+  useEffect(() => {
+    getGameCategoryAPI();
+  }, []);
+
+  const onSubmit = () => {
+    console.log('favorite', favorite);
+  };
+
   return (
     <section className="sign-up-photo mx-auto pt-lg-227 pb-lg-227 pt-130 pb-50">
       <div className="container mx-auto">
@@ -43,26 +63,28 @@ export default function SignUpPhoto() {
                   name="category"
                   className="form-select d-block w-100 rounded-pill text-lg"
                   aria-label="Favorite Game"
+                  value={favorite}
+                  onChange={(event) => setFavorite(event.target.value)}
                 >
-                  <option value="" disabled selected>
-                    Select Category
-                  </option>
-                  <option value="fps">First Person Shoter</option>
-                  <option value="rpg">Role Playing Game</option>
-                  <option value="arcade">Arcade</option>
-                  <option value="sport">Sport</option>
+                  {categories.map((category) => {
+                    return (
+                      <option key={category._id} value={category._id} selected>
+                        {category.name}
+                      </option>
+                    );
+                  })}
                 </select>
               </div>
             </div>
 
             <div className="button-group d-flex flex-column mx-auto">
-              <a
+              <button
+                type="button"
                 className="btn btn-create fw-medium text-lg text-white rounded-pill mb-16"
-                href="/sign-up-photo-success"
-                role="button"
+                onClick={onSubmit}
               >
                 Create My Account
-              </a>
+              </button>
 
               <a
                 className="btn btn-tnc text-lg color-palette-1 text-decoration-underline pt-15"
