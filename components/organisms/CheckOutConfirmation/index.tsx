6a837985cc0348eb97710c4f9c4e43f21b72ba7c
suwidnyana <grayfullbuster522@gmail.com>
2025-08-17 19:30:52 +0800

    Mempersiapkan data untuk API checkout

--- components/organisms/CheckOutConfirmation/index.tsx
+++ components/organisms/CheckOutConfirmation/index.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function CheckOutConfirmation() {
+  const [checkbox, setCheckbox] = useState(false);
+  const onSubmit = () => {
+    const dataItemLocal = localStorage.getItem('data-item');
+    const dataTopUpLocal = localStorage.getItem('data-topup');
+
+    const dataItem = JSON.parse(dataItemLocal!);
+    const dataTopUp = JSON.parse(dataTopUpLocal!);
+
+    console.log('Payment confirmed', checkbox);
+    if (!checkbox) {
+      toast('Please confirm that you have transferred the money.');
+    }
+    const data = {
+      voucher: dataItem._id,
+      nominal: dataTopUp.nominalItem._id,
+      payment: dataTopUp.paymentItem.payment._id,
+      bank: dataTopUp.paymentItem.bank._id,
+      name: dataTopUp.bankAccountName,
+      accountUser: dataTopUp.verifyID,
+    };
+    console.log('Data to submit:', data);
+  };
   return (
     <>
       <label className="checkbox-label text-lg color-palette-1">
         I have transferred the money
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={checkbox}
+          onChange={() => setCheckbox(!checkbox)}
+        />
         <span className="checkmark" />
       </label>
       <div className="d-md-block d-flex flex-column w-100 pt-50">
-        <a
+        <button
+          type="button"
           className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
-          href="/complete-checkout"
-          role="button"
+          onClick={onSubmit}
         >
           Confirm Payment
-        </a>
+        </button>
       </div>
     </>
   );
