e436f9a6616506046a209942a56f28b39107254f
suwidnyana <grayfullbuster522@gmail.com>
2025-09-11 11:43:49 +0800

    Setup private route member

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,5 +1,7 @@
 import OverviewContent from '@components/organisms/OverviewContent';
 import Sidebar from '@components/organisms/Sidebar';
+import jwtDecode from 'jwt-decode';
+import { JWTPayloadTypes, UserTypes } from 'services/data-types';
 
 export default function index() {
   return (
@@ -8,4 +10,36 @@ export default function index() {
       <OverviewContent />
     </section>
   );
+}
+
+interface GetServerSideProps {
+  req: {
+    cookies: {
+      token: string;
+    };
+  };
+}
+
+export async function getServerSideProps({ req }: GetServerSideProps) {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanent: false,
+      },
+    };
+  }
+
+  const jwttoken = Buffer.from(token, 'base64').toString('ascii');
+  const payload: JWTPayloadTypes = jwtDecode(jwttoken);
+  const userFromPayload: UserTypes = payload.player;
+  const IMG = process.env.NEXT_PUBLIC_IMG;
+  userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`;
+
+  return {
+    props: {
+      user: userFromPayload,
+    },
+  };
 }
